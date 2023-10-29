ce162bf8b70148fd7092f1626ecce6c5dcf1e585
VulcanWM <logmedha09@gmail.com>
2023-10-29 13:34:04 +0000

    added clear times button and format change of time

--- popup.js
+++ popup.js
@@ -4,25 +4,39 @@ async function changeFocus(){
         await chrome.storage.local.set({ focus: [0, 0] })
         document.getElementById("changeFocusButton").innerText = "Turn off focus"
     } else {
-        const lastResult = await chrome.storage.local.get(["focus"])
+        const lastResult = await chrome.storage.local.get(["last"])
         const last = lastResult.last;
         const newLast = [];
         if (last != undefined){
-            for (lastScore of last){
+            for (let lastScore of last){
                 newLast.push(lastScore)
             }
+        } else {
+            var element = document.getElementById("last");
+            element.innerHTML = ""
         }
         newLast.push(result.focus)
-        alert(result.focus)
         await chrome.storage.local.set({ last: newLast })
         var para = document.createElement("p");
-        var node = document.createTextNode(result.focus);
+        const minutes = result.focus[0]
+        const seconds = result.focus[1]
+        var node = document.createTextNode(`${minutes} mins ${seconds} secs`);
         para.appendChild(node);
         var element = document.getElementById("last");
         element.appendChild(para);
         await chrome.storage.local.set({ focus: false })
         document.getElementById("changeFocusButton").innerText = "Turn on focus"
     }
+}
+
+async function clearAllTimes(){
+    await chrome.storage.local.remove("last")
+    var element = document.getElementById("last");
+    element.innerHTML = ""
+    var para = document.createElement("p");
+    var node = document.createTextNode("Start your focus to show logs.");
+    para.appendChild(node);
+    element.appendChild(para);
 }
 
 async function windowLoad(){
@@ -34,18 +48,23 @@ async function windowLoad(){
     }
     const lastResult = await chrome.storage.local.get(['last']);
     const last = lastResult.last;
+    var element = document.getElementById("last");
     if (last == undefined){
         var para = document.createElement("p");
         var node = document.createTextNode("Start your focus to show logs.");
         para.appendChild(node);
-        var element = document.getElementById("last");
         element.appendChild(para);
     } else {
-        for (lastScore of last) {
+        var button = document.createElement("button")
+        button.innerText = "Clear all times"
+        button.onclick = async () => {await clearAllTimes();}
+        element.appendChild(button)
+        for (let lastScore of last) {
+            const minutes = lastScore[0]
+            const seconds = lastScore[1]
             var para = document.createElement("p");
-            var node = document.createTextNode(lastScore);
+            var node = document.createTextNode(`${minutes} mins ${seconds} secs`);
             para.appendChild(node);
-            var element = document.getElementById("last");
             element.appendChild(para);
         }
     }
